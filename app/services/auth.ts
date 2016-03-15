9870365cabb19e5a88f51d38276213fd82f7171d
Richard Tape <richard@iamfriendly.com>
2016-03-15 12:47:10 -0700

    Added the Response import (and inlined all of the imports for angular2/http)

--- app/services/auth.ts
+++ app/services/auth.ts
@@ -1,8 +1,6 @@
 import {Injectable} from "angular2/core";
-import {Http} from "angular2/http";
+import {Http, RequestOptionsArgs, Response, Headers} from "angular2/http";
 import {Inject} from "angular2/core";
-import {RequestOptionsArgs} from "angular2/http";
-import {Headers} from "angular2/http";
 //import jwtDecode = require("jwt-decode");
 import {Observable} from "rxjs/Observable";
 import 'rxjs/add/operator/map';
